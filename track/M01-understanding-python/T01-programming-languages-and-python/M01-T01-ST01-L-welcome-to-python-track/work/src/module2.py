450b5c97843979291671f58124cebac5b2612e85
suhasini <reddyssuhasini410@gmail.com>
2026-08-04 17:10:29 +0530

    hasi

--- track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/module2.py
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/module2.py
@@ -5,3 +5,9 @@ result = first_number/second_number
 print(result)
 result = first_number//second_number
 print(result)
+
+first_number = int(input())
+second_number = int(input())
+
+result = first_number+second_number
+print(f"sum: {result}")
