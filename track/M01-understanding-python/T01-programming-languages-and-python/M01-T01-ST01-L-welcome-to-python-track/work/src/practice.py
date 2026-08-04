ee8e0fb8ccb10fa0fbde4e9bf666a9be2f53ed45
suhasini <reddyssuhasini410@gmail.com>
2026-08-04 12:05:26 +0530

    srav

--- track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/practice.py
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/practice.py
@@ -1,4 +1,10 @@
-first_name = 12
-second_name = 8
-total = first_name + second_name
-print(total)
+class student:
+    def __init__(self):
+        self.name="suhasini"
+        self.age=23
+    def dance(self):
+        print("i love to dance")    
+s1 = student()
+print(s1.name)
+print(s1.age)    
+s1.dance
