78488e87cde79ff97269213058b5087b5575e4fb
suhasini <reddyssuhasini410@gmail.com>
2026-08-04 17:07:01 +0530

    hasi

--- track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/module2.py
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/module2.py
@@ -1,3 +1,7 @@
-a = 10
-b = 20
-print(a+b)
+first_number = int(input())
+second_number = int(input())
+
+result = first_number/second_number
+print(result)
+result = first_number//second_number
+print(result)
