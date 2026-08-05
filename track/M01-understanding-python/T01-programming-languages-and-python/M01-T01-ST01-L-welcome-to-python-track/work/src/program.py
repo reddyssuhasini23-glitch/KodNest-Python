fee10f696675bbc5b7e9e4fcb9898cf4331631a9
suhasini <reddyssuhasini410@gmail.com>
2026-08-05 16:13:42 +0530

    datatypes

--- track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/program.py
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/program.py
@@ -1,12 +1,20 @@
-class hero:
-    def __init__(self):
-        self.name = "prabhas"
-        self.age = 30
-        self.numOfMovies = 25
-    def handsome(self):
-        print("prbhas is handsome guy")
-h1 = hero()
-print(h1.name)
-print(h1.age)
-print(h1.numOfMovies)
-h1.handsome()
+a = 10
+print(a)
+print(type(a))
+
+b = 20.3
+print(b)
+print(type(b))
+
+c = "Suhasrav"
+print(c)
+print(type(c))
+
+d = 3+5j
+print(d)
+print(type(d))
+
+e = True or False
+print(e)
+print(type(e))
+     
