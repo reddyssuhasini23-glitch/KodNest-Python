61d4edafa5841861c9d69c78aff72d30f9588df1
suhasini <reddyssuhasini410@gmail.com>
2026-09-16 17:29:04 +0530

    hasini

--- track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/forloop.py
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/forloop.py
@@ -1,5 +1,18 @@
-n = 5
-for i in range(1, n+1):
-for j in range(i):
-print("*", end="")
-print()
+for i in range(1,14):
+    print(i)
+
+for i in range(1, 10):
+    print(i ** 2)
+
+squares = []
+for i in range(1,10):
+    squares.append(i**2)
+
+print(squares)
+
+courses = ["Python","Java","MERN","DSA"]
+course_lengths = []
+for course in courses:
+    course_lengths.append(len(course))
+
+print(course_lengths)
